cff0df48b039d87c92e4933a42aa7d4b8d7c004a
inevitableyellow <pmheard@umich.edu>
2023-04-06 11:39:21 -0400

    Initial commit

---
+++ spotifyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  spotifyApp
+//
+//  Created by paris heard on 4/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ spotifyApp/spotifyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotifyAppApp.swift
+//  spotifyApp
+//
+//  Created by paris heard on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct spotifyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
